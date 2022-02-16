b3ae96779220346ff184213a0637cabb60d68dc9
Xenovka <wendyanto18@gmail.com>
2022-02-16 14:27:32 +0700

    change: navbar hover animation

--- src/components/navbar/TheNavbar.vue
+++ src/components/navbar/TheNavbar.vue
@@ -5,6 +5,7 @@
       <li><a href="#">Home</a></li>
       <li><a href="#">About Us</a></li>
       <li><a href="#">Contact</a></li>
+      <li><a href="#">Events</a></li>
     </ul>
   </div>
 </template>
