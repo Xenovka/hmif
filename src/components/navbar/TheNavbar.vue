230448b1a890c12d6f2e27ec2c7085d9afc8d337
Xenovka <wendyanto18@gmail.com>
2022-02-16 15:01:04 +0700

    nav: add animation using gsap

--- src/components/navbar/TheNavbar.vue
+++ src/components/navbar/TheNavbar.vue
@@ -11,7 +11,28 @@
 </template>
 
 <script>
-export default {};
+import { gsap } from "gsap";
+import { onMounted } from "@vue/runtime-core";
+
+export default {
+  setup() {
+    function gsapAnimation() {
+      const navItems = document.querySelectorAll(".nav__item-container li");
+      const timeline = gsap.timeline({ defaults: { duration: 0.75 } });
+      timeline.fromTo(".nav__logo", { opacity: 0, y: -100 }, { opacity: 1, y: 0, ease: "power2.out" });
+
+      navItems.forEach((item) => {
+        timeline.fromTo(item, { y: -100, opacity: 0 }, { y: 0, opacity: 1, duration: 0.5, stagger: 0.5 }, "<50%");
+      });
+    }
+
+    onMounted(() => {
+      gsapAnimation();
+    });
+
+    return {};
+  }
+};
 </script>
 
 <style lang="scss" scoped>
