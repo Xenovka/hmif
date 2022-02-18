6f0d030c35734ec8c184cbb024cec531da504cf7
Xenovka <wendyanto18@gmail.com>
2022-02-18 19:52:15 +0700

    change: improve navbar and creating hero section

---
+++ src/components/hero/HeroSection.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hero-container">
+    <div class="hero-title-container" id="heroTitleContainer"></div>
+  </div>
+</template>
+
+<script>
+// import { gsap } from "gsap";
+
+export default {
+  setup() {
+    const heroTitleContainer = document.getElementById("heroTitleContainer");
+    function heroTitleAnimation() {
+      const heroTitle = "HMIF";
+
+      heroTitle.split("").forEach((letter) => {
+        const span = document.createElement("span");
+        span.classList.add("hero-title");
+        span.innerText = letter;
+
+        console.log(heroTitleContainer);
+      });
+    }
+
+    heroTitleAnimation();
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "HeroSection.scss";
+</style>
