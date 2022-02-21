5d583c02526b5db3c5078013a46fe37129c97fdc
Xenovka <wendyanto18@gmail.com>
2022-02-21 22:08:56 +0700

    change: hero page animation

--- src/components/hero/HeroSection.vue
+++ src/components/hero/HeroSection.vue
@@ -1,28 +1,48 @@
 <template>
   <div class="hero-container">
-    <div class="hero-title-container" id="heroTitleContainer"></div>
+    <div class="hero-title-container" id="heroTitleContainer" ref="heroTitle">
+      <span class="hero-title-animated">WELCOME</span>
+      <span class="hero-title-animated">TO</span>
+      <span class="hero-title-animated">HMIF</span>
+      <img class="hero-logo" src="../../assets/images/hmif.png" alt="logo hmif" />
+    </div>
   </div>
 </template>
 
 <script>
-// import { gsap } from "gsap";
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+import { gsap } from "gsap";
 
 export default {
   setup() {
-    const heroTitleContainer = document.getElementById("heroTitleContainer");
+    const heroTitle = ref(null);
+
     function heroTitleAnimation() {
-      const heroTitle = "HMIF";
-
-      heroTitle.split("").forEach((letter) => {
-        const span = document.createElement("span");
-        span.classList.add("hero-title");
-        span.innerText = letter;
-
-        console.log(heroTitleContainer);
+      const titleElements = document.querySelectorAll(".hero-title-animated");
+      const timeline = gsap.timeline({ defaults: { duration: 0.75 } });
+      timeline.delay(1);
+      titleElements.forEach((el) => {
+        timeline.fromTo(
+          el,
+          { opacity: 0, y: 50 },
+          { opacity: 1, y: 0, duration: 0.3, stagger: 0.1, ease: "power2.out" }
+        );
       });
+      titleElements.forEach((el) => {
+        timeline.to(el, { opacity: 0, y: -50, duration: 0.5, stagger: 0.1, delay: 0.25, ease: "power2.out" });
+      });
+      timeline.to(".hero-logo", { y: "-50%", opacity: 1, duration: 0.5, ease: "power2.out" });
+      timeline.to(".hero-logo", { opacity: 0.6, duration: 0.5 });
     }
 
-    heroTitleAnimation();
+    onMounted(() => {
+      heroTitleAnimation();
+    });
+
+    return {
+      heroTitle
+    };
   }
 };
 </script>
